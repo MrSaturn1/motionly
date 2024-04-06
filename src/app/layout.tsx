df379ab7b50a97c53eb9e1bd142b32d87a59a25e
Cheuk Lee <34779510+leecheuk@users.noreply.github.com>
2024-04-06 16:26:26 -0400

    feat: add branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@fontsource-variable/archivo";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Motionly",
@@ -14,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#131E41" />
+      </Head>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-5xl">Motionly</h1>
+    <main className="max-w-lg mx-auto py-12">
+      <img src="/brand.svg" className="mx-auto" />
+      <p className="font-normal text-2xl my-8 text-center">
+        We draft <span className="font-bold text-3xl">discovery motions</span>{" "}
+        for your litigation cases{" "}
+        <span className="underline">automagically</span>
+      </p>
     </main>
   );
 }
